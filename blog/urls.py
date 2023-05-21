52533aaffe1da5a3744488f87b1f624dffcf9937
Animish_Sharma <animishharitash@gmail.com>
2023-05-21 12:56:10 +0530

    second-commit

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-# Admin wala import on url kewal main site me hona chahiye and not in any of the apps 
-from django.urls import path , include
-from . import views
-
-urlpatterns = [
-    path('' ,views.index,name ="BlogHome")
-
-]
-
